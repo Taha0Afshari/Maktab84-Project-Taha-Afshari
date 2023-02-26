40649968128088aa547313e82290afa6e8f4993c
Taha Afshari <Taha80Afshari@gmail.com>
2023-02-26 19:08:59 +0330

    add header&footer  UI

---
+++ src/pages/AdminProductPage/index.jsx
@@ -0,0 +1,9 @@
+export const AdminProductPage = () => {
+    return ( 
+        <div className="flex justify-center items-center w-screen h-screen">
+            <div className="">
+
+            </div>
+        </div>
+     );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,10 @@
 export * from './Auth'
+export * from './AdminOrdersPage'
+export * from './AdminProductPage'
+export * from './AdminQuantityPage'
+export * from './Cart'
+export * from './CheckoutPage'
+export * from './Home'
+export * from './ProductPage'
+export * from './ProductsList'
+
