c8b256cbb19f81bd4f234a09a11cac2d42e36c6e
Taha Afshari <Taha80Afshari@gmail.com>
2023-03-01 22:12:54 +0330

    git changes

--- src/components/Productcard/index.jsx
+++ src/components/Productcard/index.jsx
@@ -9,10 +9,10 @@ export const Productcard = () => {
             <div className="flex flex-col justify-star items-cnter m-2">
                 <div className="flex justify-between text-[#555555]">
                     <span>LC-Man</span>
-                    <RxHeart className="text-2xl hover:rounded-full hover:text-red-900" />
+                    <RxHeart className="text-2xl hover:rounded-full font-semibold hover:text-red-900" />
                 </div>
                 <span className="my-1 text-lg font-bold">کت شلوار دامادی</span>
-                <span className="my-1 text-base font-bold"> 3,500,000 تومان</span>
+                <span className="my-1 text-base font-bold">3,500,000 تومان</span>
             </div>
             <button className="w-full bg-[#222222] hover:bg-black text-white p-2 rounded-b-md text-xl">افزودن به سبد خرید</button>
         </div>
