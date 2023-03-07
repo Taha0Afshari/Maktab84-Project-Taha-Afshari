59832b7692735b60634f3f3384988d6dd00d0ad0
Taha Afshari <Taha80Afshari@gmail.com>
2023-03-07 16:41:24 +0330

    git routes fix

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,14 +1,17 @@
 export const PAGE = {
-    Main: '/',
-    Home: '/home',
-    Products: '/products',
-    Product: '/product',
-    Basket: '/basket',
-    Checkouts: '/checkouts',
-    Login: '/login',
-    Admin_Order: '/admin/order',
-    Admin_Product: '/admin/product',
-    Admin_Quantity: '/admin/quantity',
+    HOME: "/",
+    EROR404: "*",
+    CATEGORY: "/categoryPage",
+    LOGIN: "/loginPage",
+    MANAGEMENT: "/Management",
+    ORDERS: "/Management/orders",
+    PRODUCTS: "/Management/products",
+    INVENTORY: "/Management/inventory",
+    COMMODITY: "/commodityPage",
+    CART: "/cartPage",
+    INFORMATION: "/informationPage",
+    PAYMENT: "/paymentPage",
+    REULT: "/resultPage"
 }
 
 // export const INTERNAL_PATHS = {
